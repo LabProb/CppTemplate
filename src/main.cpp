0564b4eb85f166c918c63d18b73ef429b42e174a
merry_berry <merryb404@gmail.com>
2026-08-02 17:41:39 +0000

    fix package

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <CppTemplate/explorer.hpp>
 #include <iostream>
 auto main() -> int {
-  char *tmp = new char('a');
-  delete tmp;
-  std::cout << *tmp;
   Explorer explorer;
   explorer.run();
   return 0;
