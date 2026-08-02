c716511468e20e783e54bd639b52939b00172c09
merry_berry <merryb404@gmail.com>
2026-08-02 08:39:13 +0000

    Fix format

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <CppTemplate/explorer.hpp>
 #include <iostream>
 auto main() -> int {
-  char* tmp = new char('a');
+  char *tmp = new char('a');
   delete tmp;
-  std::cout<<*tmp;
+  std::cout << *tmp;
   Explorer explorer;
   explorer.run();
   return 0;
